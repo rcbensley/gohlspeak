d2b8f636b02ecaa8e98df674b6d61de18665e265
rcbensley <richardbensley@gmail.com>
2023-10-20 17:12:44 +0100

    Using full vox path

--- gohlsay.go
+++ gohlsay.go
@@ -3,6 +3,8 @@ package main
 import (
 	"bytes"
 	"embed"
+	"fmt"
+	"os"
 	"time"
 
 	_ "embed"
@@ -11,26 +13,41 @@ import (
 	"github.com/hajimehoshi/go-mp3"
 )
 
-//go:embed vox/*.mp3
+//go:embed vox
 var files embed.FS
 
 func main() {
+	l := len(os.Args)
 
-	fp := "vox/attention.mp3"
-	fb, err := files.ReadFile(fp)
-	if err != nil {
-		panic(err)
+	if l <= 1 {
+		fmt.Println("no words")
+		os.Exit(1)
 	}
 
-	fr := bytes.NewReader(fb)
+	words := os.Args[1:]
 
-	d, err := mp3.NewDecoder(fr)
-	if err != nil {
-		panic("mp3.NewDecoder failed: " + err.Error())
+	a := []*mp3.Decoder{}
+
+	for _, w := range words {
+		fName := "vox/" + w + ".mp3"
+		var fb []byte
+		var err error
+		fb, err = files.ReadFile(fName)
+		if err != nil {
+			fb, _ = files.ReadFile("vox/error.mp3")
+		}
+		fr := bytes.NewReader(fb)
+		d, err := mp3.NewDecoder(fr)
+		if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
+
+		a = append(a, d)
+
 	}
 
 	op := &oto.NewContextOptions{}
-
 	op.SampleRate = 44100
 	op.ChannelCount = 2
 	op.Format = oto.FormatSignedInt16LE
@@ -41,13 +58,16 @@ func main() {
 	}
 	<-readyChan
 
-	player := otoCtx.NewPlayer(d)
-	player.Play()
-	for player.IsPlaying() {
-		time.Sleep(time.Millisecond)
-	}
-	err = player.Close()
-	if err != nil {
-		panic(err)
+	for _, w := range a {
+		player := otoCtx.NewPlayer(w)
+		player.Play()
+		for player.IsPlaying() {
+			time.Sleep(time.Millisecond)
+		}
+		err = player.Close()
+		if err != nil {
+			panic(err)
+		}
+		time.Sleep(time.Millisecond * 333)
 	}
 }
